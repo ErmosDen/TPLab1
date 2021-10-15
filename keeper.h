2e1d5d72c9d5ab3984175f8054622751f18ca1d5
ErmosDen <denisermil82@gmail.com>
2021-10-15 21:03:36 +0300

    Зафиксированы описания классов

--- keeper.h
+++ keeper.h
@@ -1,6 +1,19 @@
 #pragma once
+#include "cars.h"
+#include "worker.h"
+#include "funiture.h"
+
 class keeper
 {
-
+	keeper();
+	~keeper();
+	void push(fabric* f);
+	fabric* operator[] (const int index);
+	void save();
+	void load();
+	int getsize();
+private:
+	int size;
+	elem* head;
 };
 
